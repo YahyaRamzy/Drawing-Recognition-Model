fba290f41ccf08b2f676d1a3ef1ce7cc0522c622
YahyaRamzy <89773568+YahyaRamzy@users.noreply.github.com>
2023-04-23 08:58:29 +0200

    aaaaa

--- app.py
+++ app.py
@@ -32,7 +32,12 @@ graph = tf.compat.v1.get_default_graph()
 
 # Use pickle to load in the pre-trained model.
 with open(f'Drawing-Recognition-Model\model_cnnVGGCOMPLETE.pkl', 'rb') as f:
-        model = pickle.load(f)
+        modelVGG = pickle.load(f)
+
+# Use pickle to load in the pre-trained model.
+with open(f'Drawing-Recognition-Model\model_resnet50commp.pkl', 'rb') as f:
+        modelResnet = pickle.load(f)
+
 
 #Initializing new Flask instance. Find the html template in "templates".
 app = flask.Flask(__name__, template_folder='templates')
@@ -44,12 +49,16 @@ def home():
 
 @app.route('/vgg16')
 def vgg():
-	return render_template('draw.html')
+	return render_template('drawvgg.html')
+
+@app.route('/resnet')
+def resnet():
+	return render_template('drawresnet.html')
 
 
 #Second route : Use our model to make prediction - render the results page.
-@app.route('/predict', methods=['POST', 'GET'])
-def predict():
+@app.route('/predictvgg', methods=['POST', 'GET'])
+def predictvgg():
         
                     final_pred = None
                     #Preprocess the image : set the image to 28x28 shape
@@ -77,8 +86,49 @@ def predict():
                     vect = np.asarray(resized, dtype="uint8")
                     vect = vect.reshape(1, 1, 224, 224).astype('float32')
                     #Launch prediction
-                    model.run_eagerly = True
-                    my_prediction = model.predict(input_image)
+                    modelVGG.run_eagerly = True
+                    my_prediction = modelVGG.predict(input_image)
+                    #Getting the index of the maximum prediction
+                    print(my_prediction)
+                    index = np.argmax(my_prediction[0])
+                    #Associating the index and its value within the dictionnary
+                    final_pred = label_dict[index]
+                    query = final_pred
+                    results = search_images(query)
+                    return render_template('results.html', results =results)
+
+
+@app.route('/predictresnet', methods=['POST', 'GET'])
+def predictresnet():
+        
+                    final_pred = None
+                    #Preprocess the image : set the image to 28x28 shape
+                    #Access the image
+                    draw = request.form['url']
+                    #Removing the useless part of the url.
+                    draw = draw[init_Base64:]
+                    #Decoding
+                    draw_decoded = base64.b64decode(draw)
+                    image = np.asarray(bytearray(draw_decoded), dtype="uint8")
+                    image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+                    #Resizing and reshaping to keep the ratio.
+                    resized_image = cv2.resize(image, (180, 180))
+                    # Convert the image to a 3-channel image
+                    if len(resized_image.shape) == 2:
+                                resized_image = np.stack([resized_image] * 3, axis=-1)
+                    elif resized_image.shape[-1] == 1:
+                                resized_image = np.concatenate([resized_image] * 3, axis=-1)
+
+                        # Expand the dimensions of the image to add the batch dimension
+                    input_image = np.expand_dims(resized_image, axis=0)
+                         
+                      
+                    resized = cv2.resize(image, (180,180), interpolation = cv2.INTER_AREA)
+                    vect = np.asarray(resized, dtype="uint8")
+                    vect = vect.reshape(1, 1, 180, 180).astype('float32')
+                    #Launch prediction
+                    modelResnet.run_eagerly = True
+                    my_prediction = modelResnet.predict(input_image)
                     #Getting the index of the maximum prediction
                     print(my_prediction)
                     index = np.argmax(my_prediction[0])
